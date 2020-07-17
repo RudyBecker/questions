e02256ce307cd1b1525e4bf3df1186e8a03a19eb
zan-rosenthal <zan.rosenthal@gmail.com>
2020-07-17 16:33:26 -0500

    Reverse order of count sections

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @banana_response_count = nil
     @lincoln_response_count = nil
+    @banana_response_count = nil
 
     @recent_responses = Response.all.shuffle.take(2)
     @tagged_responses = nil
